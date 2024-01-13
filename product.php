d37982dc2c86ab46684e3da319446afacdc5da43
4thuthu <146660348+4thuthu@users.noreply.github.com>
2024-01-13 19:42:26 +0700

    Update product.php

--- product.php
+++ product.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   //Kiem tra duoi file
   $doc = strpos($image_name, ".");
   $extention = substr($image_name, $doc, 4);
-  if($extention == '.pdf'){
+  if($extention == '.jpg' | $extention == '.png'){
     move_uploaded_file($image['tmp_name'], 'images/'.$image_name);
     echo 'Bạn đã nộp tác phẩm thành công'.'<br>'.'Chúng tôi sẽ thông báo qua email cho bạn ngay khi có kết quả!';
     echo '<br>'.'<a href="images/'. $image_name .'">Xem lại tác phẩm</a>';
