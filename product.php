8ed09f816e453566db6521aa714c99e78bb34eda
4thuthu <146660348+4thuthu@users.noreply.github.com>
2024-01-16 23:41:13 +0700

    Update product.php

--- product.php
+++ product.php
@@ -1,18 +1,24 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-  
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
   $image = $_FILES['image'];
   $image_name = $image['name'];
 
-  //Kiem tra duoi file
+  if(!empty($name) && !empty($email) && !empty($image_name)){
+    //Kiem tra duoi file
   $doc = strpos($image_name, ".");
   $extention = substr($image_name, $doc, 4);
-  if($extention == '.jpg' | $extention == '.png'){
+  if($extention == ".jpg" | $extention == ".png"){
     move_uploaded_file($image['tmp_name'], 'images/'.$image_name);
-    echo 'Bạn đã nộp tác phẩm thành công'.'<br>'.'Chúng tôi sẽ thông báo qua email cho bạn ngay khi có kết quả!';
+    echo "Bạn đã nộp tác phẩm thành công."."<br>".'Chúng tôi sẽ thông báo qua email cho bạn ngay khi có kết quả!';
     echo '<br>'.'<a href="images/'. $image_name .'">Xem lại tác phẩm</a>';
   }else{
-    echo 'Sai định dạng';
+    echo 'sai định dạng';
+  }
+  }else{
+    echo "Điền đầy đủ các trường!";
   }
 }
 ?>
