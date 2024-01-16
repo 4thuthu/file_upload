97e8852059589f21e868126beccb629df34436a2
4thuthu <146660348+4thuthu@users.noreply.github.com>
2024-01-16 23:57:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-<h1 style = "text-align:center">LĂNG KÍNH THÁNG TƯ</h1><br>
-<p style = "text-align:center">*Lưu ý: tác phẩm nộp phải là file JPG hoặc PNG</p><br><br>
-<div style = 'width:400px; margin-right:auto; margin-left:auto;'>
-  <form action="product.php" method="post" enctype="multipart/form-data">
-    <input type="text" name="name" placeholder="Họ và tên">
-    <br><br>
-    <input type="email" name="email" placeholder="Email">
-    <br><br>
-    <input type="file" name="image" placeholder="Tác phẩm">
-    <br><br>
-    <input style = "text-align:center" type="submit" value="Gửi">
-  </form>
+    <h1 style = "text-align:center">LĂNG KÍNH THÁNG TƯ</h1><br>
+    <p style = "text-align:center">*Lưu ý: tác phẩm nộp phải là file JPG hoặc PNG</p><br><br>
+    <div style = 'width:400px; margin-right:auto; margin-left:auto;'>
+      <form action="product.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Họ và tên">
+        <br><br>
+        <input type="email" name="email" placeholder="Email">
+        <br><br>
+        <input type="file" name="image" placeholder="Tác phẩm">
+        <br><br>
+        <input style = "text-align:center" type="submit" value="Gửi">
+      </form>
     
 </body>
 </html>
